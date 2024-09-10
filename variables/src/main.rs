01f952d777d0439eb6c35ee4a98e030019d50717
Nagashayan <nagashayan1@gmail.com>
2024-09-10 15:22:33 -0400

    Shadowing & Constants

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,7 +3,12 @@ fn main() {
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
-    const TIME = 10*60; // only takes constant expression
+    const TIME: i32 = 10 * 60; // only takes constant expression
     let y = 10;
+    println!("The value of y is: {y}");
     let y = 10 * 2; // shadowing
+    println!("The value of y is: {y}");
+
+    let guess: u32 = "42".parse().expect("Not a number!");
+    println!("The value of guess is: {guess}");
 }
