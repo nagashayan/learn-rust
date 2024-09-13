fbdb88638165a50a338f9ae068d7245a5df39533
Nagashayan <nagashayan1@gmail.com>
2024-09-13 19:07:57 -0400

    For and while loops

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -8,4 +8,32 @@ fn main() {
         }
     };
     println!("The result: {result} and counter: {counter}");
+
+    counter = 0;
+    'outer_loop: loop {
+        println!("The counter: {counter}");
+        if counter == 5 {
+            break;
+        }
+        loop {
+            if counter == 3 {
+                break 'outer_loop;
+            } else if counter > 4 {
+                break;
+            }
+            counter += 1;
+        }
+    }
+    println!("The counter: {counter}");
+
+    let arr = [1, 2, 3, 4, 5];
+    for a in arr {
+        println!("{a}");
+    }
+
+    let mut i = 0;
+    while i < 5 {
+        println!("{i}");
+        i += 1;
+    }
 }
